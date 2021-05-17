acc3ec02809cb6fd39c12bc4e167d7ebba3e6082
Ashutosh Kumar <ashutshkumr@gmail.com>
2021-05-17 15:07:57 +0530

    no need to use bearer token

--- cowin-slots.py
+++ cowin-slots.py
@@ -4,7 +4,7 @@ import json
 
 # login to cowin using your phone number, search for your district, open dev tools using F12 and select the GET request
 # click on Headers tab and under "Request Headers", copy the contents of after "Bearer" in "authorization" key and paste it here
-BEARER_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiIzMzk4ZGQxNC01MDhmLTRkOTgtODhmOC1hYTIzYTE2N2ZiM2YiLCJ1c2VyX2lkIjoiMzM5OGRkMTQtNTA4Zi00ZDk4LTg4ZjgtYWEyM2ExNjdmYjNmIiwidXNlcl90eXBlIjoiQkVORUZJQ0lBUlkiLCJtb2JpbGVfbnVtYmVyIjo5NzQ5MDcxMjA5LCJiZW5lZmljaWFyeV9yZWZlcmVuY2VfaWQiOjM3ODQ3Njk2MzI0ODUwLCJzZWNyZXRfa2V5IjoiYjVjYWIxNjctNzk3Ny00ZGYxLTgwMjctYTYzYWExNDRmMDRlIiwidWEiOiJNb3ppbGxhLzUuMCAoWDExOyBMaW51eCB4ODZfNjQpIEFwcGxlV2ViS2l0LzUzNy4zNiAoS0hUTUwsIGxpa2UgR2Vja28pIENocm9tZS85MC4wLjQ0MzAuMjEyIFNhZmFyaS81MzcuMzYiLCJkYXRlX21vZGlmaWVkIjoiMjAyMS0wNS0xN1QwODoxMzowMi4wMzBaIiwiaWF0IjoxNjIxMjM5MTgyLCJleHAiOjE2MjEyNDAwODJ9.FdqFaEpX2SJ1hKRW8G7fXJ1D6DhVLTWn6_acNkLZz3Q"
+# BEARER_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiIzMzk4ZGQxNC01MDhmLTRkOTgtODhmOC1hYTIzYTE2N2ZiM2YiLCJ1c2VyX2lkIjoiMzM5OGRkMTQtNTA4Zi00ZDk4LTg4ZjgtYWEyM2ExNjdmYjNmIiwidXNlcl90eXBlIjoiQkVORUZJQ0lBUlkiLCJtb2JpbGVfbnVtYmVyIjo5NzQ5MDcxMjA5LCJiZW5lZmljaWFyeV9yZWZlcmVuY2VfaWQiOjM3ODQ3Njk2MzI0ODUwLCJzZWNyZXRfa2V5IjoiYjVjYWIxNjctNzk3Ny00ZGYxLTgwMjctYTYzYWExNDRmMDRlIiwidWEiOiJNb3ppbGxhLzUuMCAoWDExOyBMaW51eCB4ODZfNjQpIEFwcGxlV2ViS2l0LzUzNy4zNiAoS0hUTUwsIGxpa2UgR2Vja28pIENocm9tZS85MC4wLjQ0MzAuMjEyIFNhZmFyaS81MzcuMzYiLCJkYXRlX21vZGlmaWVkIjoiMjAyMS0wNS0xN1QwODoxMzowMi4wMzBaIiwiaWF0IjoxNjIxMjM5MTgyLCJleHAiOjE2MjEyNDAwODJ9.FdqFaEpX2SJ1hKRW8G7fXJ1D6DhVLTWn6_acNkLZz3Q"
 AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"
 # ID for Kolkata
 DISTRICT_ID = 725
@@ -20,7 +20,7 @@ response = requests.request(
     "GET", URL, verify=False, allow_redirects=True,
     headers={
         # 'Content-Type': 'application/json',
-        "authorization": "Bearer %s" % BEARER_TOKEN,
+        # "authorization": "Bearer %s" % BEARER_TOKEN,
         "user-agent": AGENT
     },
     params={
--- readme.md
+++ readme.md
@@ -10,7 +10,6 @@ env/bin/python cowin-slots.py
 
 ### NOTE
 - Do not rely entirely on the script - it works today, may not work later
-- Use your own bearer token (explained in the script)
 - Check global variables to change the filters in script
 - Limit the script execution to less than 20 times an hour
 - Running this script from an IP outside India may not work
